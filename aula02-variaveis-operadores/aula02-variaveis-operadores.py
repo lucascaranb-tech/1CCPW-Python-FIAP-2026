6cfc1068e0036430913ce65f812689d76ce4195b
lucascaranb-tech <lucascaranb@gmail.com>
2026-03-16 22:41:04 -0300

    aula 2 de python variaveis

---
+++ aula02-variaveis-operadores/aula02-variaveis-operadores.py
@@ -0,0 +1,18 @@
+print("olá mundo")
+
+print(7 + 4)
+
+
+nome = "lucas"
+print(nome)
+
+idade = 26
+print(idade)
+
+
+# input =
+nome = input("digite seu nome:")
+idade =int(input("digite a sua idade:"))
+peso = float(input("digite o seu peso:"))
+
+
---
+++ aula02-variaveis-operadores/desafios.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome:")
+print(f"Seja bem-vindo {nome}")
+
+dia = input("Digite apenas o dia do seu nascimento:")
+mes = input("Digite apenas o mês do seu nascimento:")
+ano = input("Digite apenas o ano do seu nascimento:")
+print(f"SUA data de nascimento é {dia}/{mes}/{ano}")
